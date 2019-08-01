e44035ff17b9a70c90b36a47e3da441777bdb847
Tuan Nguyen <vantuantk4@gmail.com>
2019-08-01 21:36:45 +0700

    Chapter 5: Filling in the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
-    get "static_pages/home"
-    get "static_pages/help"
-    get "static_pages/contact"
-    get "static_pages/about"
+    root "static_pages#home"
+
+    get "/help", to: "static_pages#help"
+    get "/contact", to: "static_pages#contact"
+    get "/about", to: "static_pages#about"
+    get "/signup", to: "users#new"
   end
 end
